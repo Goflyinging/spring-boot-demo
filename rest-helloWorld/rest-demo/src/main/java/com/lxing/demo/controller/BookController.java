85ec5b30578b495a82e418f2e6f0eefc39c7774e
luxing <lxcurtain@gmail.com>
2017-11-06 23:51:22 +0800

    ResponseEntity

--- rest-helloWorld/rest-demo/src/main/java/com/lxing/demo/controller/BookController.java
+++ rest-helloWorld/rest-demo/src/main/java/com/lxing/demo/controller/BookController.java
@@ -2,9 +2,11 @@ package com.lxing.demo.controller;
 
 
 import com.lxing.demo.domain.Book;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,33 +26,50 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/books")
 public class BookController {
 
-    private static final Logger logger = LoggerFactory.getLogger(BookController.class);
+  Map<String, Book> bookRepository = new ConcurrentHashMap<>();
+  public static AtomicInteger atomicInteger = new AtomicInteger(0);
 
 
-    @PostMapping
-    public Book create(@RequestBody Book book) {
-        book.setId("1");
-        return book;
+  @PostMapping
+  public ResponseEntity<Book> create(@RequestBody Book book) {
+    //生成id
+    String id = String.valueOf(atomicInteger.incrementAndGet());
+    book.setId(id);
+    bookRepository.put(id, book);
+    //返回处理结果
+    return new ResponseEntity<>(book, HttpStatus.CREATED);
+  }
+
+  @DeleteMapping("/{id:\\d+}")
+  public ResponseEntity<Book> delete(@PathVariable("id") String id) {
+    Book book = bookRepository.remove(id);
+    if (book == null) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
+    return new ResponseEntity<>(HttpStatus.NO_CONTENT);//请求收到但返回结果为空
+  }
 
-    @DeleteMapping("/{id:\\d+}")
-    public void delete(@PathVariable("id") String id) {
-        logger.info("删除用户：id{}", id);
+
+  @PutMapping("/{id:\\d+}")
+  public ResponseEntity<Book> update(@RequestBody Book book) {
+    Book originalBook = bookRepository.get(book.getId());
+    if (originalBook == null) {
+      return new ResponseEntity<>(book, HttpStatus.NOT_FOUND);
     }
+    originalBook.setAuthor(book.getAuthor());
+    originalBook.setBookName(book.getBookName());
+    originalBook.setPrice(book.getPrice());
+    bookRepository.put(originalBook.getId(), originalBook);
+    return new ResponseEntity<>(originalBook, HttpStatus.OK);
+  }
 
-
-    @PutMapping("/{id:\\d+}")
-    public Book update(@RequestBody Book book) {
-        book.setId("1");
-        return book;
+  @GetMapping("/{id:\\d+}")
+  public ResponseEntity<Book> getInfo(@PathVariable("id") String id) {
+    Book book = bookRepository.get(id);
+    if (book == null) {
+      return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
-
-    @GetMapping("/{id:\\d+}")
-    public Book getInfo(@PathVariable("id") String id) {
-        Book book = new Book();
-        book.setId(id);
-
-        return book;
-    }
+    return new ResponseEntity<>(book, HttpStatus.OK);
+  }
 
 }
